751d40e501d03137a2c1536864d0bc9d76ed9db0
KinoAR <kinoarose@gmail.com>
2017-07-22 01:09:14 -0400

    Add readme file to generate typedoc information

---
+++ doc-readme.md
@@ -0,0 +1,16 @@
+
+# RPGMakerMV Library Docs
+
+---
+
+## What This Is About
+
+This is all about **information!** This website contains **all of the documentation for the RPGMakerMV classes, globals, and more!** 
+
+## Credits
+
+* Kino: http://endlessillusoft.com/
+* LTNGames: https://ltngames.net/
+* Nio Kasgami
+* AsterAtwood: https://github.com/AsterAtwood
+* Sabakan03: https://github.com/sabakan03
--- generate-doc.sh
+++ generate-doc.sh
@@ -1,1 +1,1 @@
-typedoc --mode file --out ./doc lz-string.d.ts fpsmeter.d.ts rpg_scenes.d.ts rpg_sprites.d.ts rpg_windows.d.ts rpg_managers.d.ts rpg_core.d.ts rpg_library.d.ts rpg_objects.d.ts rpg_mv.d.ts --includeDeclarations --name "RPGMakerMV Library"
+typedoc --mode file --out ./doc lz-string.d.ts fpsmeter.d.ts rpg_scenes.d.ts rpg_sprites.d.ts rpg_windows.d.ts rpg_managers.d.ts rpg_core.d.ts rpg_library.d.ts rpg_objects.d.ts rpg_mv.d.ts --includeDeclarations --name "RPGMakerMV Library" --readme doc-readme.md
