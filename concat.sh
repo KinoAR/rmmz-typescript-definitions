023c42cb62d23b0e987c16f0240655cfacfa5e6c
KinoAR <kinoarose@gmail.com>
2017-07-24 20:32:11 -0400

    Fix package.json, add credit for regular and pixi included version

--- concat.sh
+++
@@ -1,1 +0,0 @@
-cat ../credits.txt ../libs/PIXI.d.ts ../libs/fpsmeter.d.ts ../libs/lz-string.d.ts rpg_core.d.ts rpg_library.d.ts rpg_mv.d.ts rpg_objects.d.ts rpg_managers.d.ts rpg_sprites.d.ts rpg_windows.d.ts rpg_scenes.d.ts > ../rmmv.d.ts
