930d6dc44dc2e557f70428e0d1429fd18ceacf44
KinoAR <kinoarose@gmail.com>
2017-07-26 13:30:25 -0400

    Add distribution folder for easy movement of files

--- generate-doc.sh
+++
@@ -1,1 +0,0 @@
-typedoc --mode file --out ./doc ./lib/lz-string.d.ts ./lib/fpsmeter.d.ts ./src/rpg_scenes.d.ts ./src/rpg_sprites.d.ts ./src/rpg_windows.d.ts ./src/rpg_managers.d.ts ./src/rpg_core.d.ts ./src/rpg_library.d.ts ./src/rpg_objects.d.ts ./src/rpg_mv.d.ts --includeDeclarations --name "RPGMakerMV Library" --readme doc-readme.md
